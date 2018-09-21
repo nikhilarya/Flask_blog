e7d3dbf007970ba4730177c313ef992f823e6555
nikhilarya <nikhilarya31@gmail.com>
2018-09-21 11:43:08 +0530

    project added

---
+++ flaskblog.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<h1>Home Page</h1>"
+
+
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1>"
